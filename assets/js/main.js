3b2e623423e75414816065fe37cdf15655ca697c
miracleonyenma <miracleficient@gmail.com>
2019-10-24 21:13:53 +0100

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,96 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+$(document).ready(function(){
+	
+	//Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+	
+	//Build a scene
+	
+	var firstscene = new ScrollMagic.Scene({
+		triggerElement: '#footer',
+		triggerHook: 0.9
+	})
+	.setClassToggle('.footer-content', 'move-in') //add class to #footer
+	.addIndicators({
+		
+	})
+	.addTo(controller);
 
-//Buttons 
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
 
-//counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-console.log(size);
+	//animate words
+	var wordsTxt = $('#content-txt');
+	var blob = document.getElementById("blob");
+	var loadTl = new TimelineMax();
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+	// var indicatorsWrap = document.getElementById("indicator-container");
+	// var indicators = document.getElementById("indicator-container").childNodes;
+	// var indicators1 = indicators[0];
+	// var indicators2 = indicators[1];
+	// var indicators3 = indicators[2];
+	// var indicators4 = indicators[3];
+	var indicators = $("#indicator-container");
+	var loadBlind = $(".hoves-preloader");
+	
+	loadTl
+	.to(loadBlind, 1.8, {y:"-100%"})
+	.from(blob, 1.8, {autoAlpha: 0, y: "90%"}, delay=1)
+	.from(wordsTxt, 1.8, {autoAlpha: 0, x: "-90%"}, delay=1)
 
-//button listeners
 
-nextBtn.addEventListener('click', ()=>{
-	if(counter >= carouselImages.length -1) return;
-	carouselSlide.style.transition = "transform 0.4s ease";
-	counter++;
-	console.log("you are on image " + counter );
-	carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
+
+	//Timeline to animate out and in
+
+	
+	
+	
 });
 
-prevBtn.addEventListener('click', ()=>{
-	if(counter <=0 ) return;
-	carouselSlide.style.transition = "transform 0.4s ease";
-	counter--;
-	console.log("you are on image " + counter );
-	carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+var i = 0;
+var duration = 6000; //duration
+var contentTxt = document.getElementById("content-txt"); //display text
+var words = []; //array for possible words
+var txt = document.querySelector(".txt-contain").children[0];
+var pos = 0;
+//words list
+words[0] ='Designer';
+words[1] ='"Coder"';
+words[2] ='Developer';
+words[3] ='Hi';
 
-});
 
-carouselSlide.addEventListener('transitionend', ()=>{
-	if (carouselImages[counter].id === 'lastClone' ){
-	carouselSlide.style.transition = "none";
-	counter = carouselImages.length -2;
-	carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-	console.log(counter);
-
+function changetxt(){
+	contentTxt.innerHTML = words[i];
+	contentTxt.dataset.name = words[i];
+	var wordsLength = words.length -1;
+	
+	if( i < wordsLength){
+		i++;
+		
 	}
-
-	if (carouselImages[counter].id === 'firstClone' ){
-	carouselSlide.style.transition = "none";
-	counter = carouselImages.length - counter;
-	carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-	console.log(counter);
-
+	
+	else{
+		i = 0;
 	}
-});
+	
+	setTimeout("changetxt()", duration);
+}
 
-nextBtn.classList.add('next', 'button');
-prevBtn.classList.add('prev', 'button');
+
+function animatetxt(){
+	
+}
+
+window.addEventListener("load", changetxt);
+
+
+
+
+
+
+
+
+
+
 
 
 
